5783a9a70ca562d13cdcac36df3d6f204b5afa20
Ashwin Raut <ashwinraut12345@gmail.com>
2024-03-02 18:47:35 +0530

    basic working endpoint for complete for bad apple

--- src/app.js
+++ src/app.js
@@ -1,60 +1,55 @@
 const express = require("express");
+const { parse } = require("svg-parser");
+const cmd = require("node-cmd");
+const fs = require("fs");
 
 const app = express();
 
 const PORT = 3000;
 
-app.get("/convertVideo", (req, res) => {
-  res.send("Hello, World");
+app.get("/convertVideo", async (req, res) => {
+  const frames = {};
+
+  try {
+    // convert the video into bitmap frames
+    cmd.runSync(
+      `ffmpeg -i ../videos/badapple.webm -vf fps=1 -pix_fmt bgr8 ../videos/frames/badapple%d.bmp`
+    );
+
+    // getting the number of frames generated
+    const numberOfFrames = fs.readdirSync("../videos/frames/").length;
+
+    for (let i = 1; i <= numberOfFrames; i++) {
+      // convert the bitmap frames into svgs using potrace
+      cmd.runSync(`potrace ../videos/frames/badapple${i}.bmp -b svg`);
+
+      // take the svg file, parse it and extract all the paths from the svg file
+
+      // extracting the contents of the svg file
+      const svgContent = fs.readFileSync(`../videos/frames/badapple${i}.svg`, {
+        encoding: "utf-8",
+      });
+
+      frames[i] = [];
+
+      // parse the svg files content
+      const parsedSvg = parse(svgContent);
+       // get the g tag which contains all the path tags
+      const pathTags = parsedSvg.children[0].children[1].children;
+
+      // add the paths to the final frames object
+      pathTags.forEach((path) => {
+        const svgPathString = path.properties.d; // get the path string
+        frames[i].push(svgPathString.replace(/\n/g, " ")); // get rid of all the \n
+      });
+    }
+  } catch (e) {
+    console.log(e);
+  }
+
+  res.send(frames);
 });
 
 app.listen(PORT, () => {
   console.log(`server listening on http://localhost:${PORT}`);
 });
-
-// const ffmpeg = require("ffmpeg");
-
-// const getVideo = async () => {
-//   try {
-//     const video = await new ffmpeg("../videos/badapple.webm");
-
-//     video.addCommand("-vf");
-//     video.addCommand("fps=1");
-//     video.addCommand("-pix_fmt", "bgr8");
-//     video.addCommand("../videos/frames/output%d.bmp");
-
-//     video.save();
-//   } catch (e) {
-//     console.log(e);
-//   }
-// };
-
-// getVideo();
-
-// const { parse } = require("svg-parser");
-
-// const Snap = require(`imports-loader?this=>window,fix=>module.exports=0!snapsvg/dist/snap.svg.js`);
-// const Snap = require("snapsvg");
-
-// const fs = require("fs");
-// const cmd = require("node-cmd");
-// cmd.runSync("potrace ../videos/frames/output55.bmp -b svg");
-
-// // getting all the paths from a svg
-// const svgContent = fs.readFileSync("../videos/frames/output55.svg", {
-//   encoding: "utf-8",
-// });
-
-// const svgPaths = [];
-
-// const parsedSvg = parse(svgContent);
-// const paths = parsedSvg.children[0].children[1].children;
-
-// paths.forEach((path) => {
-//   const svgPathString = path.properties.d;
-//   svgPaths.push(svgPathString);
-// });
-
-// console.log(svgPaths);
-
-
